1cfcc13acdcdd7418ef27e7bec04fcd825345433
unknown <ennbadger@st.ug.edu.gh>
2024-05-18 17:53:43 +0100

    finished second task

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,23 +1,33 @@
 let numbers = [];
 
-//console.log(numbers);
-
 const processArray = (numbers) => {
     const modifiedNums = numbers.map((number) => {
         if(number % 2 === 0){
-            return Math.pow(number,2);
+            return Math.pow(number,2);   //squares the number
         }
         else {
-            return number * 3;
+            return number * 3;           //triples the number(times 3)
         }
     })
 
     console.log(modifiedNums);
 }
 
-processArray([2,4,5]);
+processArray([2,4,5,6,7,9]);
 
+const formatArrayStrings = (stringArr,modifiedNums) => {
+    const modifiedStringArr = stringArr.map((string,index) => {
+        if(modifiedNums[index] % 2 === 0){
+            return string.toUpperCase();
+        }
+        else{
+            return string.toLowerCase();
+        }
+    })
+    console.log(modifiedStringArr);
+}
 
+formatArrayStrings(["hello", "kitty", "world", "port"], [2, 3, 5, 7]);
 
 
 
